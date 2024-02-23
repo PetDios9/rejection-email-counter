50e6e5c221fb63345e2a5f278264155fd102552e
Peter Diosdado <diosdadopeter@gmail.com>
2024-02-22 16:55:22 -0800

    text: changed verbage on limiter input

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import email
 EMAIL_ADDRESS = input("What is your email address?: ")
 PASSWORD = input("What is your password?: ")
 IMAP = input("What is your IMAP server? (if unsure, google your email host + imap to find out): ")
-LIMITER = int(input("Enter the number of last emails you would like to check. This is optional. If nothing is entered, every email in your inbox will be checked. This could take some time.: ")) 
+LIMITER = int(input("Enter the number of last recieved emails you would like to check. This is optional. If nothing is entered, every email in your inbox will be checked. This could take some time.: ")) 
 PHRASES = ['unfortunately', 'move forward with other candidates', 'not to move forward', 'will not be moving forward']
 
 mail = imaplib.IMAP4_SSL(IMAP)
